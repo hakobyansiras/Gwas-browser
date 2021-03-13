c0537cc4be7d0d4e4b504fe178570041e2789c30
hakobyansiras <sirashakobyan@gmail.com>
2021-03-13 16:54:38 +0400

    Initial commit

---
+++ ui.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(googleVis)
+library(DT)
+library(data.table)
+
+shinyUI(
+  
+  fluidPage(theme = "cerulean.css",
+    
+    titlePanel (span("GWAS Catalog", style = "color:blue")),
+    sidebarLayout(
+      sidebarPanel(
+        singleton(tags$head(HTML(
+          '
+    <script type="text/javascript">
+    $(document).ready(function() {
+    $("#data_file").attr("disabled", "true").attr("onclick", "return false;");
+    
+    Shiny.addCustomMessageHandler("download", function(message) {
+    $("#data_file").attr("disabled", "true").attr("onclick", "return false;").html(
+    "<i class=\\"fa fa-download\\"></i> Download");
+    });
+    Shiny.addCustomMessageHandler("download_ready", function(message) {
+    $("#data_file").removeAttr("disabled").removeAttr("onclick").html(
+    "<i class=\\"fa fa-download\\"></i> Download search results");
+    });
+    })
+    </script>
+    '
+        ))),
+        tags$head(tags$script('$(document).keyup(function(event){
+            if(event.keyCode == 13){
+                      $("#go").click();
+            }
+        });
+        ')),
+        
+        textInput("a1", label = h5("Please insert searching item")
+        ),
+        actionButton("go",label =  "Search",icon = icon("fa fa-search")),
+        
+        downloadButton("data_file")
+      ),
+      
+      mainPanel(
+        div(style='width: 700px', tabsetPanel(
+          tabPanel(title='Initial information',textOutput("text"),
+                   textOutput("text2"),tags$head(tags$style("#text2{color: #FBB117;font-size: 20px;}")),
+                   tags$head(tags$style("#text{color: #728C00;font-size: 20px;}")),
+                   dataTableOutput("table2"),
+                   htmlOutput("view"),dataTableOutput("table1")),
+          tabPanel(title="Genes and SNPs",textOutput("text1"),
+                   div(dataTableOutput("table3"), style = "width: 700px"),br(),
+                   div(dataTableOutput("table4"), style = "width: 700px")),
+          tabPanel(title="Ontology annotations",textOutput("text3"),dataTableOutput("table5")),
+          tabPanel(title="Gwas table",dataTableOutput("table")),
+          id="tabs"))
+      )
+    )
+  ))
